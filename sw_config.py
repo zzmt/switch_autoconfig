5ed2ee425c1294490be7356fd5bedf0f604e304f
zhangzhang03 <zhangzhang03@meituan.com>
2020-06-04 14:40:27 +0800

    v4

--- sw_config.py
+++ sw_config.py
@@ -70,6 +70,7 @@ class SSH(object):
             print not_done
             if self.client_conn.recv_ready():
                 output += self.client_conn.recv(self.buffer).decode('utf-8')
+                print output
                 if '<' in  output or '#' in output or '~' in  output:
                     not_done = False
             else:
